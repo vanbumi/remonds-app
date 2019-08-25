2a2fcff0a52ea3a64d27733bba2f30496622145b
vanbumi <jsp.dio@gmail.com>
2019-08-26 06:19:50 +0700

    materi bab 20 done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -148,7 +148,25 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route		DELETE api/profile
+// @desc		Delete profile, user dan posts
+// @access	Private
 
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await Profile.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User berhasil dihapus' });
+
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // export route
 module.exports = router;
