117c84b786e5b1da81fb50e2d67bcdb351fd73c0
vanbumi <jsp.dio@gmail.com>
2019-08-26 06:16:02 +0700

    materi bab 18 done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -77,7 +77,42 @@ router.post(
       profileFields.skills = skills.split(',').map(skill => skill.trim());
     }
 
+    // build social object
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+
+        // udpate profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // create profile
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
 
-    // export route
-    module.exports = router;
+
+// export route
+module.exports = router;
