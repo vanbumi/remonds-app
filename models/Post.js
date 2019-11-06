ee37e16eb4c7c4e2aeeb6ca0e4576679bd305c28
vanbumi <jsp.dio@gmail.com>
2019-11-06 17:10:57 +0700

    materi 25 to 27 done

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+const mongose = require('mongoose');
+const Schema = mongose.Schema;
+
+const PostSchema = new Schema({
+  // user ref
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      // relation/reference
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongose.model('post', PostSchema);
