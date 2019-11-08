f8b7fefb72961137de558b8c0cf2279e2671af40
vanbumi <jsp.dio@gmail.com>
2019-11-08 18:06:21 +0700

    materi bab31 done

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React, { Fragment } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
-)
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     // copy dan paste navbar dari remonds-theme disini, dan rubah class menjadi className
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> Remonds App</a>
+        <a href="/"><i className="fas fa-code"></i> Remonds App</a>
       </h1>
       <ul>
         <li><a href="profiles.html">Members</a></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><Link to="register">Register</Link></li>
+        <li><Link to="login">Login</Link></li>
       </ul>
     </nav>
   )
