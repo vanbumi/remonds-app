072ed69bbacfcbfe584e5861c2935575e12af297
vanbumi <jsp.dio@gmail.com>
2020-03-10 18:57:23 +0700

    materi bab 39 done

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS, 
     REGISTER_FAIL,
     USER_LOADED,
-	AUTH_ERROR 
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL 
 } from '../actions/types';
 
 const initialState = {
@@ -26,6 +28,7 @@ export default function(state = initialState, action) {
                 user: payload
             }
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:    
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
@@ -34,7 +37,8 @@ export default function(state = initialState, action) {
                 loading: false
             };
         case REGISTER_FAIL:
-        case AUTH_ERROR:    
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return {
                 ...state,
