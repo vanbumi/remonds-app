dbbbc9b6825c04667a7caf50b67d6e8323006096
vanbumi <jsp.dio@gmail.com>
2020-03-27 18:50:05 +0700

    materi bab 44 done

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 // Cek apakah toke tersedia?
 if(localStorage.token) {
@@ -37,6 +38,10 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute 
+                exact 
+                path="/create-profile" 
+                component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
