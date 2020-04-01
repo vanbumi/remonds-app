e46ed8048a490e584e6bb80afdbd526abda7c36b
vanbumi <jsp.dio@gmail.com>
2020-04-01 20:07:05 +0700

    materi bab 47 done

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 // Cek apakah toke tersedia?
 if(localStorage.token) {
@@ -46,7 +48,15 @@ const App = () => {
               <PrivateRoute 
                 exact 
                 path="/edit-profile" 
-                component={EditProfile} />  
+                component={EditProfile} />
+              <PrivateRoute 
+                exact 
+                path="/add-experience" 
+                component={AddExperience} />   
+              <PrivateRoute 
+                exact 
+                path="/add-education" 
+                component={AddEducation} />    
             </Switch>
           </section>
         </Fragment>
