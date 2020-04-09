54d79eb9767adf18314580304d1859679f71ef0a
vanbumi <jsp.dio@gmail.com>
2020-04-09 16:49:02 +0700

    materi bab 53 done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({ 
     getProfileById,
@@ -28,13 +30,16 @@ const Profile = ({
                     <Link to='/profiles' className='btn btn-light'>
                         Back To Profiles
                     </Link>
-                    {/* {auth.isAuthenticated &&
+                    {auth.isAuthenticated &&
                         auth.loading === false &&
                         auth.user._id === profile.user._id && (
                         <Link to='/edit-profile' className='btn btn-dark'>
                             Edit Profile
                         </Link>
-                    )} */}
+                    )}
+                    <div class='profile-grid my-1'>
+                        <ProfileTop profile={profile} />                   
+                    </div>
                 </Fragment>
             ) }
         </Fragment>
