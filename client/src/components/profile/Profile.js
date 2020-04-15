a087047abb1ac0511e86420259ec90f9953ea130
vanbumi <jsp.dio@gmail.com>
2020-04-15 12:09:14 +0700

    materi bab 54 dan 55 done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,9 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ 
     getProfileById,
@@ -39,7 +42,46 @@ const Profile = ({
                     )}
                     <div class='profile-grid my-1'>
                         <ProfileTop profile={profile} />                   
-                        <ProfileAbout profile={profile} />                   
+                        <ProfileAbout profile={profile} />
+                            
+                        <div class="profile-exp bg-white p-2">
+                            <h2 class="text-primary">Experience</h2>
+                            {
+                                profile.experience.length > 0 ? (
+                                    <Fragment>
+                                        {
+                                            profile.experience.map(experience => (
+                                                <ProfileExperience 
+                                                    key={experience._id}
+                                                    experience={experience}
+                                                />
+                                            ))
+                                        }
+                                    </Fragment>
+                                ) : (
+                                    <h4>No Experience Provide</h4>
+                                )
+                            }
+                        </div>
+
+                        <div className='profile-edu bg-white p-2'>
+                            <h2 className='text-primary'>Education</h2>
+                            {profile.education.length > 0 ? (
+                                <Fragment>
+                                    {profile.education.map(education => (
+                                        <ProfileEducation
+                                            key={education._id}
+                                            education={education}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No education Provide</h4>
+                            )}
+                        </div> 
+                        {profile.githubusername && (
+                            <ProfileGithub username={profile.githubusername} />
+                        )}                  
                     </div>
                 </Fragment>
             ) }
