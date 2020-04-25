87d862b7419aaf19aa94211fb34480198952506a
vanbumi <jsp.dio@gmail.com>
2020-04-25 12:12:37 +0700

    on going bab62 but need to merge to bab61 first

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,6 +6,8 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types';
 
 // Get posts
@@ -104,3 +106,47 @@ export const addPost = formData => async dispatch => {
         });
     }
 }
+
+// Add Comment
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment Removed', 'danger'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
